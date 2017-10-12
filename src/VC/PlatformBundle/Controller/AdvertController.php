137099bf038c9eaf59adbb1f7537b535d6c49070
Your Name <you@example.com>
2017-10-12 17:02:59 +0200

    controleurs

--- src/VC/PlatformBundle/Controller/AdvertController.php
+++ src/VC/PlatformBundle/Controller/AdvertController.php
@@ -3,24 +3,40 @@
 namespace VC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse; // Ne pas oublier  ce use!!!
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
-    {
-      $content = $this
-          ->get('templating')
-          ->render('VCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
+  // On récupère tous les paramètres en arguments de la méthode
+      public function indexAction()
+      {
+        //on veut avoir l'url de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'vc_platform_view' ,  // 1er argument : le nom de la router
+             array('id' => 5)     // 2e argument : les valeurs des paramètres
+        );
 
-        return new Response($content);
-    }
-    public function goodbyeAction()
-    {
-      $content = $this
-          ->get('templating')
-          ->render('VCPlatformBundle:Advert:goodbye.html.twig');
+        return new Response("L'URL de l'annonce d'id 5 est :".$url);
+      }
 
-        return new Response($content);
-    }
+
+      public function viewSlugAction($slug, $year, $format)
+      {
+          return new Response(
+              "On pourrait afficher l'annonce correspondant au
+              slug '".$slug."', créée en ".$year." et au format ".$format."."
+          );
+      }
+  // La route fait appel à OCPlatformBundle:Advert:view,
+  // on doit donc définir la méthode viewAction.
+  // On donne à cette méthode l'argument $id, pour
+  // correspondre au paramètre {id} de la route
+
+  public function viewAction($id)
+  {
+    // que l'on renvoie du JSON et non du HTML
+    return new JsonResponse(array('id' => $id));
+
+  }
 }
